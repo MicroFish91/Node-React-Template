f3032e2b3e3730372b12c98158a8f47c1aa5c824
MicroFish91 <mwfisher91@gmail.com>
2021-07-01 12:09:28 -0700

    updated class components

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import App from './components/App';
 import BaseLayout from './components/layouts/BaseLayout';
 import reducer from './reducers';
 import Hooks from './template/hooks';
+import ClassComponent from './template/classComponent';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -18,6 +19,7 @@ ReactDOM.render(
           <Switch>
             <Route exact path="/" component={App} />
             <Route exact path="/hooks" component={Hooks} />
+            <Route exact path="/class" component={ClassComponent} />
           </Switch>
         </BaseLayout>
       </Router>
