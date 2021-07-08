042e3a455347699a93a324240b055860cfbb96e4
Matthew Fisher <mwfisher91@gmail.com>
2021-07-08 15:10:05 -0700

    Updated dependencies

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const debug = require('debug')('app:startup');  // set env 'export DEBUG='app:startup'
-const express = require('express');
-const morgan = require('morgan');
+const debug = require("debug")("app:startup"); // set env 'export DEBUG='app:startup'
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 // Log Traffic
-if (app.get('env') === 'development') {
-  app.use(morgan('tiny')); 
-  debug('Morgan enabled...');
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  debug("Morgan enabled...");
 }
 
 // Req.body
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 const port = process.env.PORT | "3001";
 
